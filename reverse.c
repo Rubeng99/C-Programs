cfed76bab3ce45f7eec1c2597431095b49ddd2f3
Ruben <78626115+Rubeng99@users.noreply.github.com>
2025-09-17 11:54:33 -0700

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -13,31 +13,20 @@ char *toBinary(uint8_t num)
 
     binary[bits] = '\0';
 
-    // for (int i = bits - 1; i >= 0; i--)
-    // {
-    //     binary[i] = (num & 1) ? '1' : '0';
-    //     num >>= 1;
-    // }
     for (int i = 0; i < bits - 1; i++)
     {
         binary[i] = (num & 1) ? '1' : '0';
         num >>= 1;
     }
+    //free(binary);
     return binary;
 }
 
 int main()
 {
     int num = 43261596;
-    char str[20];
     printf("%s\n", toBinary(num));
     int number = (int)strtol(toBinary(num), NULL, 2);
     printf("%d", number);
-    // printf("%lu\n", sizeof(u_int8_t));
-    // sprintf(str, "%d", num);
-
-    // char *bin = toBinary(x);
-    // printf("%u in binary = %s\n", x, bin);
-    // free(bin);
     return 0;
 }
